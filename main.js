8d755e9db36dddabf9a959dedbb22d5895ce8ed7
Leon Styven <j.stca03@gmail.com>
2022-09-19 23:44:01 -0500

    Insertar menus desplegables y carrito

--- main.js
+++ main.js
@@ -1,8 +1,44 @@
 const menuEmail = document.querySelector(".navbar-email");
+const IconHamMenu = document.querySelector(".HamMenuIcon");
+const shoppingCartIcon = document.querySelector(".navbar-shopping-cart");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
+const aside = document.querySelector(".product-detail");
+
+
+
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
+IconHamMenu.addEventListener('click', toggleMobileMenu);
+shoppingCartIcon.addEventListener('click', toggleAsideCart);
 
 function toggleDesktopMenu(){
-    desktopMenu.classList.toggle("innactive");
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    desktopMenu.classList.toggle("inactive");
+}
+function toggleMobileMenu(){
+    const isAsideClosed = aside.classList.contains('inactive');
+
+    if(!isAsideClosed){
+        aside.classList.add('inactive');
+    }
+    mobileMenu.classList.toggle("inactive");
+}
+
+function toggleAsideCart(){
+    const isMobileMenuClosed = mobileMenu.classList.contains('inactive');
+    const isDesktopMenuClosed = desktopMenu.classList.contains('inactive');
+
+    if(!isMobileMenuClosed){
+        mobileMenu.classList.add('inactive');
+    }
+    if(!isDesktopMenuClosed){
+        desktopMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
 }
